a4b88d6ad000928a37bd10bf407608a178607052
yankiweiss <yakovw2706@gmail.com>
2026-03-16 17:33:18 -0400

    update

---
+++ client/src/Homepage.jsx
@@ -0,0 +1,31 @@
+import logo from './assets/Full-Logo-01.png';
+
+function Homepage () {
+    return (
+    <>
+
+    <div className='home-page'>
+
+   <section>
+
+    <div className='logo-container'>
+
+    <img src={logo} className='logo' alt='logo'></img>
+    </div>
+   </section>
+
+   <h1 className='sub-title'>kosher bungalows that fit your needs</h1>
+
+   </div>
+
+    <div className='home-page'>
+
+   
+
+   </div>
+    
+    </>
+    )
+}
+
+export default Homepage;
